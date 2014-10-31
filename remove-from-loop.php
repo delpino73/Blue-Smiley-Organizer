2dacc0f390009908e02e76e8d0333a7614cccf1e
Oliver Antosch <antosch@gmail.com>
2014-10-31 20:54:12 +0000

    change to new db commands

--- remove-from-loop.php
+++ remove-from-loop.php
@@ -10,31 +10,31 @@ $value=isset($_GET['value']) ? $_GET['value'] : '';
 
 if ($knowledge_id) {
 
-$rand=mt_rand(-100,100); $now=time();
+	$rand=mt_rand(-100,100); $now=time();
 
-if ($value=='5mi') { $new_value=$now+$rand+300; } # 5 mins
-else if ($value=='15mi') { $new_value=$now+$rand+900; } # 15 mins
-else if ($value=='60mi') { $new_value=$now+$rand+3600; } # 60 mins
-else if ($value=='1d') { $new_value=$now+$rand+86400; } # 1 day
-else if ($value=='3d') { $new_value=$now+$rand+259200; } # 3 days
-else if ($value=='7d') { $new_value=$now+$rand+604800; } # 7 days
-else if ($value=='14d') { $new_value=$now+$rand+1209600; } # 14 days
-else if ($value=='30d') { $new_value=$now+$rand+2592000; } # 30 days
-else if ($value=='3mo') { $new_value=$now+$rand+7776000; } # 3 months
-else if ($value=='6mo') { $new_value=$now+$rand+15552000; } # 6 months
-else if ($value=='12mo') { $new_value=$now+$rand+31104000; } # 12 months
-else if ($value=='bl') { $new_value=-1; } # block
-else { echo 'error'; exit; }
+	if ($value=='5mi') { $new_value=$now+$rand+300; } # 5 mins
+	else if ($value=='15mi') { $new_value=$now+$rand+900; } # 15 mins
+	else if ($value=='60mi') { $new_value=$now+$rand+3600; } # 60 mins
+	else if ($value=='1d') { $new_value=$now+$rand+86400; } # 1 day
+	else if ($value=='3d') { $new_value=$now+$rand+259200; } # 3 days
+	else if ($value=='7d') { $new_value=$now+$rand+604800; } # 7 days
+	else if ($value=='14d') { $new_value=$now+$rand+1209600; } # 14 days
+	else if ($value=='30d') { $new_value=$now+$rand+2592000; } # 30 days
+	else if ($value=='3mo') { $new_value=$now+$rand+7776000; } # 3 months
+	else if ($value=='6mo') { $new_value=$now+$rand+15552000; } # 6 months
+	else if ($value=='12mo') { $new_value=$now+$rand+31104000; } # 12 months
+	else if ($value=='bl') { $new_value=-1; } # block
+	else { echo 'error'; exit; }
 
-$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET value=$new_value,shown=shown+1,last_shown=$now,word_loop=0 WHERE word_id='$knowledge_id' AND user='$userid' AND category_id=0");
+	$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET value=$new_value,shown=shown+1,last_shown=$now,word_loop=0 WHERE word_id='$knowledge_id' AND user='$userid' AND category_id=0");
 
-$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=1 WHERE word_loop=0 AND user=$userid AND category_id=0 AND value > -1 AND value < $now ORDER BY value LIMIT 1"); # find a new word from active words that is due and add to loop
+	$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=1 WHERE word_loop=0 AND user=$userid AND category_id=0 AND value > -1 AND value < $now ORDER BY value LIMIT 1"); # find a new word from active words that is due and add to loop
 
-$aff_rows=mysql_affected_rows();
+	$aff_rows=mysqli_affected_rows($base_instance->db_link);
 
-if ($aff_rows==0) { increase_loop_words(1); } # if nothing found add a new word to active words and add to loop
+	if ($aff_rows==0) { increase_loop_words(1); } # if nothing found add a new word to active words and add to loop
 
-echo '<head>',_CSS_NAV,'<meta http-equiv="refresh" content="10;url=status.php"></head><font size="2">Removed from Loop</font>';
+	echo '<head>',_CSS_NAV,'<meta http-equiv="refresh" content="10;url=status.php"></head><font size="2">Removed from Loop</font>';
 
 }
 
@@ -56,19 +56,19 @@ function increase_loop_words($how_many) {
 
 	for ($index=1; $index <= sizeof($data); $index++) {
 
-	$ID=$data[$index]->ID;
+		$ID=$data[$index]->ID;
 
-	$data2=$base_instance->get_data('SELECT word_id FROM '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' WHERE word_id='.$ID.' AND user='.$userid.' AND category_id=0');
+		$data2=$base_instance->get_data('SELECT word_id FROM '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' WHERE word_id='.$ID.' AND user='.$userid.' AND category_id=0');
 
-	if ($data2) { unset($data2); } else {
+		if ($data2) { unset($data2); } else {
 
-	$base_instance->query('INSERT INTO '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' (word_id, value, shown, user, word_loop, category_id) VALUES ('.$ID.',0,0,'.$userid.',1,0)');
+			$base_instance->query('INSERT INTO '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' (word_id, value, shown, user, word_loop, category_id) VALUES ('.$ID.',0,0,'.$userid.',1,0)');
 
-	$added_new_words++;
+			$added_new_words++;
 
-	}
+		}
 
-	if ($added_new_words > $how_many) { return; }
+		if ($added_new_words > $how_many) { return; }
 
 	}
 
--- show-knowledge-flashcards.php
+++ show-knowledge-flashcards.php
@@ -20,51 +20,51 @@ $loop_words=30;
 
 if (!empty($_GET['delete_id'])) {
 
-$delete_id=$_GET['delete_id'];
+	$delete_id=$_GET['delete_id'];
 
-$base_instance->query("DELETE FROM {$base_instance->entity['KNOWLEDGE']['MAIN']} WHERE ID='$delete_id' AND user='$userid'");
+	$base_instance->query("DELETE FROM {$base_instance->entity['KNOWLEDGE']['MAIN']} WHERE ID='$delete_id' AND user='$userid'");
 
-$base_instance->query("DELETE FROM {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} WHERE word_id='$delete_id' AND user='$userid'");
+	$base_instance->query("DELETE FROM {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} WHERE word_id='$delete_id' AND user='$userid'");
 
 }
 
 if (!empty($_GET['last_id'])) {
 
-$last_id=$_GET['last_id'];
+	$last_id=$_GET['last_id'];
 
-if ($value=='kp') {
+	if ($value=='kp') {
 
-	$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET shown=shown+1,last_shown=$now WHERE word_id='$last_id' AND user='$userid' AND category_id='$category_id'");
+		$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET shown=shown+1,last_shown=$now WHERE word_id='$last_id' AND user='$userid' AND category_id='$category_id'");
 
-}
+	}
 
-else if (!empty($value)) {
+	else if (!empty($value)) {
 
-	$rand=mt_rand(-100,100);
+		$rand=mt_rand(-100,100);
 
-	if ($value=='5mi') { $new_value=$now+$rand+300; } # 5 mins
-	else if ($value=='15mi') { $new_value=$now+$rand+900; } # 15 mins
-	else if ($value=='60mi') { $new_value=$now+$rand+3600; } # 60 mins
-	else if ($value=='1d') { $new_value=$now+$rand+86400; } # 1 day
-	else if ($value=='3d') { $new_value=$now+$rand+259200; } # 3 days
-	else if ($value=='7d') { $new_value=$now+$rand+604800; } # 7 days
-	else if ($value=='14d') { $new_value=$now+$rand+1209600; } # 14 days
-	else if ($value=='30d') { $new_value=$now+$rand+2592000; } # 30 days
-	else if ($value=='3mo') { $new_value=$now+$rand+7776000; } # 3 months
-	else if ($value=='6mo') { $new_value=$now+$rand+15552000; } # 6 months
-	else if ($value=='12mo') { $new_value=$now+$rand+31104000; } # 12 months
-	else if ($value=='bl') { $new_value=-1; } # block
-	else { echo 'error'; exit; }
+		if ($value=='5mi') { $new_value=$now+$rand+300; } # 5 mins
+		else if ($value=='15mi') { $new_value=$now+$rand+900; } # 15 mins
+		else if ($value=='60mi') { $new_value=$now+$rand+3600; } # 60 mins
+		else if ($value=='1d') { $new_value=$now+$rand+86400; } # 1 day
+		else if ($value=='3d') { $new_value=$now+$rand+259200; } # 3 days
+		else if ($value=='7d') { $new_value=$now+$rand+604800; } # 7 days
+		else if ($value=='14d') { $new_value=$now+$rand+1209600; } # 14 days
+		else if ($value=='30d') { $new_value=$now+$rand+2592000; } # 30 days
+		else if ($value=='3mo') { $new_value=$now+$rand+7776000; } # 3 months
+		else if ($value=='6mo') { $new_value=$now+$rand+15552000; } # 6 months
+		else if ($value=='12mo') { $new_value=$now+$rand+31104000; } # 12 months
+		else if ($value=='bl') { $new_value=-1; } # block
+		else { echo 'error'; exit; }
 
-	$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET value=$new_value,shown=shown+1,last_shown=$now,word_loop=0 WHERE word_id='$last_id' AND user='$userid' AND category_id='$category_id'");
+		$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET value=$new_value,shown=shown+1,last_shown=$now,word_loop=0 WHERE word_id='$last_id' AND user='$userid' AND category_id='$category_id'");
 
-	$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=1 WHERE word_loop=0 AND user=$userid AND category_id=$category_id AND value > -1 AND value < $now ORDER BY value LIMIT 1"); # find a new word from active words that is due and add to loop
+		$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=1 WHERE word_loop=0 AND user=$userid AND category_id=$category_id AND value > -1 AND value < $now ORDER BY value LIMIT 1"); # find a new word from active words that is due and add to loop
 
-	$aff_rows=mysql_affected_rows();
+		$aff_rows=mysqli_affected_rows($base_instance->db_link);
 
-	if ($aff_rows==0) { increase_loop_words(1); } # if nothing found add a new word to active words and add to loop
+		if ($aff_rows==0) { increase_loop_words(1); } # if nothing found add a new word to active words and add to loop
 
-}
+	}
 
 } else { $last_id=''; }
 
@@ -88,8 +88,8 @@ if ($total_loop_words < $loop_words) { # not enough loop words
 
 	if ($aff_rows==0) {
 
-	$diff=$loop_words-$total_loop_words;
-	increase_loop_words($diff);
+		$diff=$loop_words-$total_loop_words;
+		increase_loop_words($diff);
 
 	} # if nothing found add a new word to active words and add to loop
 
@@ -97,37 +97,37 @@ if ($total_loop_words < $loop_words) { # not enough loop words
 
 if ($total_loop_words > $loop_words) { # too many loop words
 
-$diff=$total_loop_words-$loop_words;
+	$diff=$total_loop_words-$loop_words;
 
-$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=0 WHERE word_loop=1 AND user='$userid' AND category_id='$category_id' ORDER BY shown DESC LIMIT $diff");
+	$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=0 WHERE word_loop=1 AND user='$userid' AND category_id='$category_id' ORDER BY shown DESC LIMIT $diff");
 
 }
 
 
 if (empty($go_back)) {
 
-$data=$base_instance->get_data("SELECT * FROM {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} WHERE user=$userid AND category_id=$category_id AND value > -1 AND value < $now AND word_loop=1 ORDER BY last_shown LIMIT 1"); # get next word in loop to show it
+	$data=$base_instance->get_data("SELECT * FROM {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} WHERE user=$userid AND category_id=$category_id AND value > -1 AND value < $now AND word_loop=1 ORDER BY last_shown LIMIT 1"); # get next word in loop to show it
 
 #echo 'hereee<br>';
 
-if (!$data) { # no words in the loop
+	if (!$data) { # no words in the loop
 
 #echo 'here 2 <br>';
 
-$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=1 WHERE user=$userid AND category_id=$category_id AND value > -1 AND value < $now ORDER BY value LIMIT $loop_words");
+		$base_instance->query("UPDATE {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} SET word_loop=1 WHERE user=$userid AND category_id=$category_id AND value > -1 AND value < $now ORDER BY value LIMIT $loop_words");
 
-$aff_rows=mysql_affected_rows();
+		$aff_rows=mysqli_affected_rows($base_instance->db_link);
 
-	if ($aff_rows < $loop_words) { # not found enough active words for loop
+		if ($aff_rows < $loop_words) { # not found enough active words for loop
 
-	$diff=$loop_words-$aff_rows;
-	increase_loop_words($diff);
+			$diff=$loop_words-$aff_rows;
+			increase_loop_words($diff);
+
+		}
+
+		$data=$base_instance->get_data("SELECT * FROM {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} WHERE user=$userid AND category_id=$category_id AND value > -1 AND word_loop=1 ORDER BY last_shown LIMIT 1"); # get next word in loop to show it
 
 	}
-
-$data=$base_instance->get_data("SELECT * FROM {$base_instance->entity['KNOWLEDGE']['FLASHCARDS']} WHERE user=$userid AND category_id=$category_id AND value > -1 AND word_loop=1 ORDER BY last_shown LIMIT 1"); # get next word in loop to show it
-
-}
 
 } else { # user has clicked "go back"
 
@@ -139,49 +139,49 @@ $data=$base_instance->get_data("SELECT * FROM {$base_instance->entity['KNOWLEDGE
 
 if (!empty($data)) {
 
-$word_id=$data[1]->word_id;
-$shown=$data[1]->shown;
+	$word_id=$data[1]->word_id;
+	$shown=$data[1]->shown;
 
 } else {
 
-$base_instance->show_message('Finished','No knowledge saved or no knowledge in the queue, check again later.'); exit;
+	$base_instance->show_message('Finished','No knowledge saved or no knowledge in the queue, check again later.'); exit;
 
 }
 
 if ($data[1]->last_shown==0) { $last_shown_text='never'; }
 else {
 
-$seconds=$now-$data[1]->last_shown;
+	$seconds=$now-$data[1]->last_shown;
 
-if ($seconds < 60) { $last_shown_text=$seconds.' secs'; }
-else if ($seconds < 3600) {
+	if ($seconds < 60) { $last_shown_text=$seconds.' secs'; }
+	else if ($seconds < 3600) {
 
-$mins=round($seconds/60);
+		$mins=round($seconds/60);
 
-if ($mins==1) { $last_shown_text=$mins.' min'; }
-else { $last_shown_text=$mins.' mins'; }
+		if ($mins==1) { $last_shown_text=$mins.' min'; }
+		else { $last_shown_text=$mins.' mins'; }
 
-}
+	}
 
-else if ($seconds < 86400) {
+	else if ($seconds < 86400) {
 
-$hours=round($seconds/3600);
+		$hours=round($seconds/3600);
 
-if ($hours==1) { $last_shown_text=$hours.' hour'; }
-else { $last_shown_text=$hours.' hours'; }
+		if ($hours==1) { $last_shown_text=$hours.' hour'; }
+		else { $last_shown_text=$hours.' hours'; }
 
-}
+	}
 
-else {
+	else {
 
-$days=round($seconds/86400);
+		$days=round($seconds/86400);
 
-if ($days==1) { $last_shown_text=$days.' day'; }
-else { $last_shown_text=$days.' days'; }
+		if ($days==1) { $last_shown_text=$days.' day'; }
+		else { $last_shown_text=$days.' days'; }
 
-}
+	}
 
-$last_shown_text.=' ago';
+	$last_shown_text.=' ago';
 
 }
 
@@ -189,13 +189,13 @@ $last_shown_text.=' ago';
 
 if ($word_id > 0) {
 
-$data=$base_instance->get_data('SELECT * FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE ID='.$word_id);
+	$data=$base_instance->get_data('SELECT * FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE ID='.$word_id);
 
 #echo 'SELECT * FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE ID='.$word_id;
 
-$title=$data[1]->title;
-$text=$data[1]->text;
-$word_category_id=$data[1]->category;
+	$title=$data[1]->title;
+	$text=$data[1]->text;
+	$word_category_id=$data[1]->category;
 
 #echo "title $title"; exit;
 
@@ -222,11 +222,11 @@ $all_text.='<table cellspacing=0 cellpadding=5 border=0 width="100%"><tr><td ali
 
 if (!empty($text)) {
 
-$text=convert_square_bracket($text);
-$text=$base_instance->insert_links($text);
-$text=nl2br($text);
+	$text=convert_square_bracket($text);
+	$text=$base_instance->insert_links($text);
+	$text=nl2br($text);
 
-$all_text.=$text.'<p>';
+	$all_text.=$text.'<p>';
 
 }
 
@@ -331,10 +331,10 @@ text-decoration:underline;
 </style>';
 
 $html_instance->add_parameter(
-array(
-'TEXT_CENTER'=>$all_text,
-'HEAD'=>"$stylesheet $head"
-));
+	array(
+		'TEXT_CENTER'=>$all_text,
+		'HEAD'=>"$stylesheet $head"
+	));
 
 $html_instance->process();
 
@@ -352,11 +352,11 @@ function increase_loop_words($how_many) {
 
 	if ($category_id > 0) {
 
-	$sql='SELECT ID FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE user='.$userid.' AND category='.$category_id.' ORDER BY RAND()';
+		$sql='SELECT ID FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE user='.$userid.' AND category='.$category_id.' ORDER BY RAND()';
 
 	} else {
 
-	$sql='SELECT ID FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE user='.$userid.' ORDER BY RAND()';
+		$sql='SELECT ID FROM '.$base_instance->entity['KNOWLEDGE']['MAIN'].' WHERE user='.$userid.' ORDER BY RAND()';
 
 	}
 
@@ -364,19 +364,19 @@ function increase_loop_words($how_many) {
 
 	for ($index=1; $index <= sizeof($data); $index++) {
 
-	$ID=$data[$index]->ID;
+		$ID=$data[$index]->ID;
 
-	$data2=$base_instance->get_data('SELECT word_id FROM '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' WHERE word_id='.$ID.' AND user='.$userid.' AND category_id='.$category_id);
+		$data2=$base_instance->get_data('SELECT word_id FROM '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' WHERE word_id='.$ID.' AND user='.$userid.' AND category_id='.$category_id);
 
-	if ($data2) { unset($data2); } else {
+		if ($data2) { unset($data2); } else {
 
-	$base_instance->query('INSERT INTO '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' (word_id, value, shown, user, word_loop, category_id) VALUES ('.$ID.',0,0,'.$userid.',1,'.$category_id.')');
+			$base_instance->query('INSERT INTO '.$base_instance->entity['KNOWLEDGE']['FLASHCARDS'].' (word_id, value, shown, user, word_loop, category_id) VALUES ('.$ID.',0,0,'.$userid.',1,'.$category_id.')');
 
-	$added_new_words++;
+			$added_new_words++;
 
-	}
+		}
 
-	if ($added_new_words > $how_many) { return; }
+		if ($added_new_words > $how_many) { return; }
 
 	}
 
